9b647e3e8733d86275126bae58cb895ee89066a4
miltonlopezjr <miltonathm@hotmail.com>
2018-10-18 12:45:47 -0400

    todo

--- services/web/src/constants.js
+++ services/web/src/constants.js
@@ -1,2 +1,2 @@
 export const AUTH_TOKEN = 'auth-token';
-export const mapboxgl_accessToken = 'pk.eyJ1IjoidHJlbnRnb2luZyIsImEiOiJjam11bDQwdGwyeWZ5M3FqcGFuaHRxd3Q2In0.UyaQAvC0nx08Ih7-vq3wag';
+export const mapboxgl_accessToken = pr;
